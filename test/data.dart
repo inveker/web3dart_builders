c9be2f4f82a676fe49c3e298ae2b1329b2c74c10
inveker <alex.korolev.1995@gmail.com>
2023-05-11 13:49:06 +0300

    tests

--- test/data.dart
+++ test/data.dart
@@ -565,4 +565,78 @@ class Contract extends _i1.GeneratedContract {
   }
 }
 ''',
+  r'''
+[
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "serviceType",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "serviceId",
+        "type": "string"
+      }
+    ],
+    "name": "verifyServiceOwnership",
+    "outputs": [],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]''': r'''
+// Generated code, do not modify. Run `build_runner build` to re-generate!
+// @dart=2.12
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:web3dart/web3dart.dart' as _i1;
+
+final _contractAbi = _i1.ContractAbi.fromJson(
+  '[{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"string","name":"serviceType","type":"string"},{"internalType":"string","name":"serviceId","type":"string"}],"name":"verifyServiceOwnership","outputs":[],"stateMutability":"view","type":"function"}]',
+  'Contract',
+);
+
+class Contract extends _i1.GeneratedContract {
+  Contract({
+    required _i1.EthereumAddress address,
+    required _i1.Web3Client client,
+    int? chainId,
+  }) : super(
+          _i1.DeployedContract(
+            _contractAbi,
+            address,
+          ),
+          client,
+          chainId,
+        );
+
+  /// The optional [atBlock] parameter can be used to view historical data. When
+  /// set, the function will be evaluated in the specified block. By default, the
+  /// latest on-chain block will be used.
+  Future<void> verifyServiceOwnership(
+    BigInt tokenId,
+    String serviceType,
+    String serviceId, {
+    _i1.BlockNum? atBlock,
+  }) async {
+    final function = self.abi.functions[0];
+    assert(checkSignature(function, '2ecff2f6'));
+    final params = [
+      tokenId,
+      serviceType,
+      serviceId,
+    ];
+    final response = await read(
+      function,
+      params,
+      atBlock,
+    );
+  }
+}
+''',
 };

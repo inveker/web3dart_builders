4160a3cc8fed24e6e1e9bd5758abe458667f91a2
inveker <alex.korolev.1995@gmail.com>
2023-05-13 18:17:50 +0300

    add event_overloading test

--- test/data.dart
+++ test/data.dart
@@ -639,4 +639,167 @@ class Contract extends _i1.GeneratedContract {
   }
 }
 ''',
+  r'''
+[
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "value",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "data",
+        "type": "bytes"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  }
+]''': r'''
+// Generated code, do not modify. Run `build_runner build` to re-generate!
+// @dart=2.12
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:web3dart/web3dart.dart' as _i1;
+import 'dart:typed_data' as _i2;
+
+final _contractAbi = _i1.ContractAbi.fromJson(
+  '[{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"data","type":"bytes"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]',
+  'Contract',
+);
+
+class Contract extends _i1.GeneratedContract {
+  Contract({
+    required _i1.EthereumAddress address,
+    required _i1.Web3Client client,
+    int? chainId,
+  }) : super(
+          _i1.DeployedContract(
+            _contractAbi,
+            address,
+          ),
+          client,
+          chainId,
+        );
+
+  /// Returns a live stream of all Transfer events emitted by this contract.
+  Stream<Transfer> transferEvents({
+    _i1.BlockNum? fromBlock,
+    _i1.BlockNum? toBlock,
+  }) {
+    final event = self.event('Transfer');
+    final filter = _i1.FilterOptions.events(
+      contract: self,
+      event: event,
+      fromBlock: fromBlock,
+      toBlock: toBlock,
+    );
+    return client.events(filter).map((_i1.FilterEvent result) {
+      final decoded = event.decodeResults(
+        result.topics!,
+        result.data!,
+      );
+      return Transfer(
+        decoded,
+        result,
+      );
+    });
+  }
+
+  /// Returns a live stream of all Transfer$2 events emitted by this contract.
+  Stream<Transfer$2> transfer$2Events({
+    _i1.BlockNum? fromBlock,
+    _i1.BlockNum? toBlock,
+  }) {
+    final event = self.event('Transfer');
+    final filter = _i1.FilterOptions.events(
+      contract: self,
+      event: event,
+      fromBlock: fromBlock,
+      toBlock: toBlock,
+    );
+    return client.events(filter).map((_i1.FilterEvent result) {
+      final decoded = event.decodeResults(
+        result.topics!,
+        result.data!,
+      );
+      return Transfer$2(
+        decoded,
+        result,
+      );
+    });
+  }
+}
+
+class Transfer {
+  Transfer(
+    List<dynamic> response,
+    this.event,
+  )   : from = (response[0] as _i1.EthereumAddress),
+        to = (response[1] as _i1.EthereumAddress),
+        value = (response[2] as BigInt),
+        data = (response[3] as _i2.Uint8List);
+
+  final _i1.EthereumAddress from;
+
+  final _i1.EthereumAddress to;
+
+  final BigInt value;
+
+  final _i2.Uint8List data;
+
+  final _i1.FilterEvent event;
+}
+
+class Transfer$2 {
+  Transfer$2(
+    List<dynamic> response,
+    this.event,
+  )   : from = (response[0] as _i1.EthereumAddress),
+        to = (response[1] as _i1.EthereumAddress),
+        value = (response[2] as BigInt);
+
+  final _i1.EthereumAddress from;
+
+  final _i1.EthereumAddress to;
+
+  final BigInt value;
+
+  final _i1.FilterEvent event;
+}
+''',
 };
